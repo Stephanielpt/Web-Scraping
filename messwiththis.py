0d26c11f4796efbc2ace7bde4c3aff72c895dd09
Stephanielpt <stephanielampotang@gmail.com>
2018-03-14 12:11:37 -0400

    working new number and linking to personal web

--- messwiththis.py
+++ messwiththis.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client #SMS API Package
 import time
+from twilio.twiml.messaging_response import MessagingResponse
 
 ###TWILIO STUFF
 account_sid = "AC3c602a60289e0d8b78c843ab743090e1" #Your Twilio account ID
@@ -8,11 +9,13 @@ auth_token = "70a0b92d0cf1d2478dfc5e1c376ed0ec"    #Your secret API Token
 client = Client(account_sid, auth_token)
 gotatext = True
 while 1:
-    if gotatext:
-       gotatext = True
-    if gotatext:
-       msg = client.messages.create(to="3528704348", from_="3524152946", body="Update! Here you go:http://highbrow.com") #Will send SMS to your phone number
-       print ("SMS Sent Thanks")
-       
-       time.sleep(20) #sleep for 20sec : 2 hours
-       gotatext = False
+    def sms_reply():
+        """Respond to incoming calls with a simple text message."""
+        # Start our TwiML response
+        resp = MessagingResponse()
+
+        # Add a message
+        resp.message("The Robots are coming! Head for the hills!")
+        print("did u even do anythingggg")
+        return str(resp)
+    quit()
