c7f1ef02c4cc0ac0c0808ee4b440e7c49def76b4
Stephanie Lampotang <slampota@usc.edu>
2018-03-14 17:01:03 -0400

    Update messwiththis.py

--- messwiththis.py
+++ messwiththis.py
@@ -3,8 +3,8 @@ import time
 from twilio.twiml.messaging_response import MessagingResponse
 
 ###TWILIO STUFF
-account_sid = "AC3c602a60289e0d8b78c843ab743090e1" #Your Twilio account ID
-auth_token = "70a0b92d0cf1d2478dfc5e1c376ed0ec"    #Your secret API Token
+account_sid = "" #Your 
+auth_token = ""    #Your Token
  
 client = Client(account_sid, auth_token)
 gotatext = True
